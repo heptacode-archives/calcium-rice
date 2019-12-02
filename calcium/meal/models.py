3384710ca6aca767ef56f9f38efe203cf7899189
jinhyeokfang <jinhyeokfang@gmail.com>
2019-12-02 18:37:51 +0900

    add template

--- calcium/meal/models.py
+++ calcium/meal/models.py
@@ -1,10 +1,1 @@
 from django.db import models
-
-class Meal(models.Model):
-    meal = models.CharField()
-    allergyCodes = models.CharField()
-    allergicFoods = models.CharField()
-    img = models.URLField(max_length=200, default='https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/SunrinInternetHighSchool.png/900px-SunrinInternetHighSchool.png')
-
-    def __str__(self):
-        return self.name
--- calcium/meal/urls.py
+++ calcium/meal/urls.py
@@ -1,8 +1,1 @@
 from django.urls import path
-from meal import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('/today', views.today, name='today'),
-    path('/yesterday', views.yesterday, name='yesterday'),
-]
