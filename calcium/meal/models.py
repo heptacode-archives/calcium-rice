78707bfb4aca73a3ff218642616b67517e106574
heptacode <admin@hyunwoo.kim>
2019-12-02 18:04:33 +0900

    Update

--- calcium/meal/models.py
+++ calcium/meal/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Meal(models.Model):
+    meal = models.CharField()
+    allergyCodes = models.CharField()
+    allergicFoods = models.CharField()
+    img = models.URLField(max_length=200, default='https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/SunrinInternetHighSchool.png/900px-SunrinInternetHighSchool.png')
+
+    def __str__(self):
+        return self.name
--- calcium/meal/views.py
+++ calcium/meal/views.py
@@ -1,3 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import get_object_or_404, render
